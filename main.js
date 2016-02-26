f1cf04c32d9b32f9c1ff98645e646e3d85a31202
Shruti Arora <shruti.arora89@icloud.com>
2016-02-25 20:41:39 -0800

    minor changes

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+
 var express = require("express");
 
-var fs=require('fs')
+var fs=require('fs');
+var http = require('http');
+
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5001));
 
 app.get('/', function(request, response){
 fs.readFile('index.html', 'utf8', function (errors, contents){
