a4616b111811d1eda6c0f73cf89c8d1c9cdb85cd
Shruti Arora <shruti.arora89@icloud.com>
2016-02-25 20:24:53 -0800

    added fs

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var express = require("express");
+
+var fs=require('fs')
 var app = express();
 app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(request, response){
- response.send("<h1> Hello world </h1>");
+fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
 });
 app.listen(app.get('port'), function(){
  console.log("listening on port ", app.get('port'));
